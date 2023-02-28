c72c5ff21a73553621de4c8b2f3312c5ebe80990
anna shishkina <ancho0002@gmail.com>
2023-02-28 21:59:27 +0300

    add Program

--- Program.cs
+++ Program.cs
@@ -5,3 +5,30 @@
 При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 */
 
+
+string[] StringValidation(string[] array)
+{
+    string[] new_massive = new string[array.Length];
+    int new_massive_index = 0;
+    for(int index = 0; index < array.Length; index++)
+    {
+        if (array[index].Length <= 3)
+        {
+            new_massive[new_massive_index] = array[index];
+            new_massive_index++;
+        }
+    }
+    return new_massive;
+}
+
+void PrintArray (string[] array)
+{
+    for(int index = 0; index < array.Length; index++)
+    {
+        Console.WriteLine (array[index]);
+    } 
+}
+
+string[] array = {"hello","2","world",":-)"};
+string[] res = StringValidation(array);
+PrintArray(res);
